0df23550bc6e499fc71f7ae362ee1a77071e5cbd
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-09-23 14:04:24 -0500

    corregir scroll en el layout

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -2,7 +2,7 @@ import Header from 'components/Header';
 import Footer from 'components/Footer';
 import React from 'react';
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
     <div className='mainContainer'>
       <Header />
